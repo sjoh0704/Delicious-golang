725da0fe836d5c432aa1f51d737a360eeeabd0be
seung <sjoh0704@daum.net>
2021-12-30 15:26:06 +0900

    add detail

--- pointer.go
+++ pointer.go
@@ -10,7 +10,7 @@ type Data struct{
 }
 
 func main(){
-	// pointer 기본 
+	// 1. pointer 기본 
 	var p *int // pointer 변수 선언, pointer 변수의 기본값을 nil이다. 
 	var a int  // 변수 공간 생성 
 	p = &a  // p에 a의 주소를 대입
@@ -20,7 +20,7 @@ func main(){
 	fmt.Println(*p)
 
 
-	//pointer는 같은 값을 가르킬 수 있다.
+	//2. pointer는 같은 값을 가르킬 수 있다.
 	b := 10
 	var p1, p2 *int
 	p1 = &b
@@ -29,12 +29,13 @@ func main(){
 	fmt.Println(*p2) // 20
 	// p1을 변경했음에도 p2가 바뀌는 것은 p1과 p2가 같은 값을 가르키기 때문이다. 
 
-	// 조심해야할점
+
+	// 3. 조심해야할점
 	data := Data{"k", "v1"}
 	fmt.Println(data)
 	
 	// 잘못된 경우
-	setDataWrong(data, "kk", "vv") // rkqt
+	setDataWrong(data, "kk", "vv") // 실패
 	fmt.Println(data) // 값이 변경되지 않은채로 출력 => data의 주소가 아닌 값을 넣었기 때문 
 
 	// 올바른 경우 
@@ -42,7 +43,7 @@ func main(){
 	fmt.Println(data) // 올바르게 값이 변경되어 출력 
 	
 
-	// 인스턴스 생성1
+	// 4. 인스턴스 생성1
 	var p3 *Data = &Data{} // 구조체의 빈 인스턴스 생성 
 	var p4 *Data = p3 // 다른 포인터가 같은 인스턴스를 가르키는 경우 
 	fmt.Println(*p3)
@@ -51,7 +52,7 @@ func main(){
 	fmt.Println(*p3)
 
 	//인스턴스 생성2
-	var p5 = new(Data) // new를 사용할때는 무조건 기본값으로 초기화된다. 
+	var p5 *Data = new(Data) // new를 사용할때는 무조건 기본값으로 초기화된다. 
 	p5.key = "k5"
 	p5.value = "v5"
 	fmt.Println(*p5)
@@ -64,17 +65,20 @@ func main(){
 
 }
 
+//객체 생성함수 
 func newData(key string, value string) *Data{
 	data := Data{key, value} // 이상해보일 수 있다. => 일반적으로 지역변수는 stack에 저장되고 함수가 끝나면 pop되어 사라진다.  
 							// Go에서는 탈출 분석을 하여 stack 대신 heap에 저장시키며 쓰임이 다하면 사라진다.  
 	return &data
 }
 
+// 잘못된 경우
 func setDataWrong(data Data, key string, value string){
 	data.key = key
 	data.value = value
 }
 
+// 올바른 경우 
 func setDataRight(data *Data, key string, value string){
 	(*data).key = key // 이렇게 하는 게 옳지만
 	data.value = value // go에서는 편의상 좌측과 같이도 쓴다. 
