344103ee216adcce11132a717bdba3508dd1c65b
seung <sjoh0704@daum.net>
2022-01-10 17:07:20 +0900

    error handling without error keyword

---
+++ errorHandling/errorhandling1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func WriteFile(filename string, line string) error{
+	file, err := os.Create(filename)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintln(file, line)
+	return err
+}
+
+func ReadFile(filename string)(string, error){
+	file, err := os.Open(filename)
+	if err != nil{
+		return "", err
+	}
+
+	defer file.Close()
+	rd := bufio.NewReader(file)
+	line, _ := rd.ReadString('\n')
+
+	return line, nil
+}
+func main(){
+
+	filename := "data.txt"
+	line, err := ReadFile(filename)
+	if err != nil{
+		err = WriteFile(filename, "This is the first line")
+		if err != nil{
+			fmt.Println("Error: Creating file fails")
+			return
+		}
+		return 
+	}
+
+	line, err = ReadFile(filename)
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println("파일 내용:", line)
+}
